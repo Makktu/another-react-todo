c4b6b1e6edc488bd040b7e7d1413a43cf8f3230f
Makktu <johnmcna@gmail.com>
2023-02-24 20:03:31 +0000

    first commit training project

---
+++ README.md
@@ -0,0 +1,3 @@
+# React2Do
+
+And this is yet another React Training exercise. This time the project is a personal sandbox rather than a 3rd-party thing from the bowels of 'tutorial hell'. I will invent as I go, looking things up as I need to.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+export default () => {
+  const helloVar = "Not another one of these...";
+  return (
+    <>
+      <div>
+        <h1>{helloVar}</h1>
+      </div>
+    </>
+  );
+};
