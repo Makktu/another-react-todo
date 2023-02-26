4e79994cd9a711b4ef34722bbe9fa092171343f4
Makktu <johnmcna@gmail.com>
2023-02-26 03:30:20 +0000

    adding button done things

--- src/DisplayToDos.jsx
+++ src/DisplayToDos.jsx
@@ -1,13 +1,27 @@
+import React, { useState } from "react";
+
 import classes from "./DisplayToDos.module.css";
 
 const DisplayToDos = (props) => {
+  const [doneTask, setDoneTask] = useState();
+
+  const doneHandler = (buttonId) => {
+    setDoneTask(buttonId);
+  };
+
   return (
     <div className={classes.todoDisplayBox}>
       <ul>
         {props.todoList.map((todo) => (
           <li key={todo.id}>
             {todo.todo}
-            <button className={classes.listBtn}>X</button>
+            <button
+              className={classes.listBtn}
+              id={todo.id}
+              onClick={() => doneHandler(todo.id)}
+            >
+              X
+            </button>
           </li>
         ))}
       </ul>
