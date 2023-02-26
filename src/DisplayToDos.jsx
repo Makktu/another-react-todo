65dd1910cd3ec298a66b5adcc7f9824273335ae6
Makktu <johnmcna@gmail.com>
2023-02-26 03:01:19 +0000

    list added

--- src/DisplayToDos.jsx
+++ src/DisplayToDos.jsx
@@ -2,13 +2,16 @@ import classes from "./DisplayToDos.module.css";
 
 const DisplayToDos = (props) => {
   return (
-    <>
-      <div className={classes.todoDisplayBox}>
-        {props.todoList.map((todo) => {
-          <p key={todo.id}>{todo.todo}</p>;
-        })}
-      </div>
-    </>
+    <div className={classes.todoDisplayBox}>
+      <ul>
+        {props.todoList.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button className={classes.listBtn}>X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
